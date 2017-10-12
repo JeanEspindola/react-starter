ee7459426c850431eeca996323267297dfa8c1c8
JeanEspindola <jean.espindola@gmail.com>
2017-10-12 14:42:10 -0300

    Initializing Project Adding main files. Adding first component file. Changing gitignore.

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class Example extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>A Simple React Component Example</h1>
+            </div>
+        );
+    }
+}
+
+module.exports = Example;
