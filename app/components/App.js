271acdfb5352e2ec5c5dea86943e5e64f33538be
JeanEspindola <jean.espindola@gmail.com>
2017-10-15 16:41:38 -0200

    Configuring JEST + Enzyme - package.json Rolling changes back.

--- app/components/App.js
+++ app/components/App.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'whatwg-fetch';
-
-const url = 'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=Spider&apikey=f70d9eaceac3d19f806f53e46f0a25ff';
 
 export class App extends React.Component {
-/*
-    loadData() {
-        fetch(url)
-            .then(function(response) {
-                return response.json();
-            }).then(function(json) {
-                console.log('parsed json', json.data.results)
-            }).catch(function(ex) {
-                console.log('parsing failed', ex)
-            })
-    }
-
-    componentDidMount() {
-        this.loadData();
-    }*/
-
     render() {
         return (
             <div>
