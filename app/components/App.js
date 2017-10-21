a4b954983d0839561b98e9f2ce0c26e6ce9f373a
JeanEspindola <jean.espindola@gmail.com>
2017-10-21 17:38:28 -0200

    ESLint Completing style guide configuration. Fixing lint errors.

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-export class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>A Simple React Component Example</h1>
-            </div>
-        );
-    }
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>A Simple React Component Example</h1>
+      </div>
+    );
+  }
 }
